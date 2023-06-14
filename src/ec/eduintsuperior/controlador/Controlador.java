1a6403386cb39db10df277ce3f9964c59c38395b
Jhonatan <Jhonatan@DESKTOP-ST21IOU>
2023-06-14 12:19:58 -0500

    Ejercicio programacion version 1.0

--- src/ec/eduintsuperior/controlador/Controlador.java
+++ src/ec/eduintsuperior/controlador/Controlador.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.intsuperior.controlador;
 
 import ec.edu.intsuperior.modelo.Cliente;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Erika
- */
+
 public class Controlador {
 
     Cliente c;
--- src/ec/eduintsuperior/modelo/Persona.java
+++ src/ec/eduintsuperior/modelo/Persona.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ec.edu.intsuperior.modelo;
 
-/**
- *
- * @author Erika
- */
+
 public class Persona {
     private String ci;
     private String apellido1;
